eb8018508cf75e2cf8702322205c69278a2a9ea8
Biswaranjan Padhy <padhybiswaranjan@live.com>
2016-09-02 10:13:44 +0530

    Initial Commit

---
+++ diffiehellman/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  diffiehellman
+//
+//  Created by Biswaranjan Padhy on 02/09/16.
+//  Copyright © 2016 Biswaranjan Padhy. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
